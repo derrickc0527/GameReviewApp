4b31f6ab037e58869f89e92066aa3761436587c1
Wai loon Chong <derrick@Wais-iMac.T-mobile.com>
2016-03-22 22:49:26 -0400

    heroku deployment

--- GameReviewApp/urls.py
+++ GameReviewApp/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.views.generic import RedirectView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^reviews/', include('reviews.urls', namespace="reviews")),
+    url(r'^$',RedirectView.as_view(url='/reviews/'), name='home'),
 
 ]
