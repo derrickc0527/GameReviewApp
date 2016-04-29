02dd02e5c8d0a7cf039dc7944d9841dcd2d8ad29
Wai loon Chong <derrick@Wais-iMac.T-mobile.com>
2016-04-29 15:39:00 -0400

    changes #1

--- GameReviewApp/urls.py
+++ GameReviewApp/urls.py
@@ -18,11 +18,30 @@ from django.contrib import admin
 from django.views.generic import RedirectView
 
 urlpatterns = [
-    url(r'^reviews/', include('reviews.urls', namespace="reviews")),
-    url(r'^$',RedirectView.as_view(url='/reviews/'), name='home'),
-    url(r'^accounts/', include('registration.backends.simple.urls')),
-    url('^accounts/', include('django.contrib.auth.urls', namespace="auth")),
-    #url(r'^logout/$', 'django.contrib.auth.views.logout',name='logout',kwargs={'template_name': 'registration/logout.html'}),
+    url(r'^$', 'reviews.views.index'),
+    url(r'^login$', 'reviews.views.login_view'),
+    url(r'^logout$', 'reviews.views.logout_view'),
+    url(r'^signup$', 'reviews.views.signup'),
+    url(r'^ribbits$', 'reviews.views.public'),
+    url(r'^submit$', 'reviews.views.submit'),
+    url(r'^users/$', 'reviews.views.users'),
+    url(r'^users/(?P<username>\w{0,30})/$', 'reviews.views.users'),
+    url(r'^follow$', 'reviews.views.follow'),
+    #url(r'^reviews$', 'ribbit_app.reviews.follow'),
+    #review
+    url(r'^review/(?P<review_id>[0-9]+)/$', 'reviews.views.review_detail', name='review_detail'),
+    url(r'^game$', 'reviews.views.game_list', name='game_list'),
+    url(r'^game/(?P<game_id>[0-9]+)/$', 'reviews.views.game_detail', name='game_detail'),
+    url(r'^game/(?P<game_id>[0-9]+)/add_review/$', 'reviews.views.add_review', name='add_review'),
+    url(r'^review/user/(?P<username>\w+)/$', 'reviews.views.user_review_list', name='user_review_list'),
+    url(r'^review/user/$', 'reviews.views.user_review_list', name='user_review_list'),
+    # url(r'^ribbit/', include('ribbit.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    # url(r'^admin/', include(admin.site.urls)),
     url(r'^admin/', admin.site.urls),
 
 ]
