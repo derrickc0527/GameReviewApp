787150aeaea924b56e4ee12bac935acb30769bf0
Wai loon Chong <derrick@Wais-iMac.T-mobile.com>
2016-04-29 15:44:14 -0400

    changes #3

--- reviews/forms.py
+++ reviews/forms.py
@@ -1,6 +1,9 @@
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth.models import User
+from django import forms
+from django.utils.html import strip_tags
+from ribbit_app.models import Ribbit, Review
 from django.forms import ModelForm, Textarea
-from reviews.models import Review
-
 
 class ReviewForm(ModelForm):
     class Meta:
@@ -9,3 +12,59 @@ class ReviewForm(ModelForm):
         widgets = {
             'comment': Textarea(attrs={'cols': 40, 'rows': 15}),
         }
+
+
+class UserCreateForm(UserCreationForm):
+    email = forms.EmailField(required=True, widget=forms.widgets.TextInput(attrs={'placeholder': 'Email'}))
+    first_name = forms.CharField(required=True, widget=forms.widgets.TextInput(attrs={'placeholder': 'First Name'}))
+    last_name = forms.CharField(required=True, widget=forms.widgets.TextInput(attrs={'placeholder': 'Last Name'}))
+    username = forms.CharField(widget=forms.widgets.TextInput(attrs={'placeholder': 'Username'}))
+    password1 = forms.CharField(widget=forms.widgets.PasswordInput(attrs={'placeholder': 'Password'}))
+    password2 = forms.CharField(widget=forms.widgets.PasswordInput(attrs={'placeholder': 'Password Confirmation'}))
+
+    def is_valid(self):
+        form = super(UserCreateForm, self).is_valid()
+        for f, error in self.errors.iteritems():
+            if f != '__all__':
+                self.fields[f].widget.attrs.update({'class': 'error', 'value': strip_tags(error)})
+        return form
+
+    def clean_username(self):
+        username = self.cleaned_data.get('username')
+        return username
+
+    def clean_password2(self):
+        password2 = self.cleaned_data.get('password2')
+        return password2
+
+    class Meta:
+        fields = ['email', 'username', 'first_name', 'last_name', 'password1',
+                  'password2']
+        model = User
+
+
+class AuthenticateForm(AuthenticationForm):
+    username = forms.CharField(widget=forms.widgets.TextInput(attrs={'placeholder': 'Username'}))
+    password = forms.CharField(widget=forms.widgets.PasswordInput(attrs={'placeholder': 'Password'}))
+
+    def is_valid(self):
+        form = super(AuthenticateForm, self).is_valid()
+        for f, error in self.errors.iteritems():
+            if f != '__all__':
+                self.fields[f].widget.attrs.update({'class': 'error', 'value': strip_tags(error)})
+        return form
+
+
+class RibbitForm(forms.ModelForm):
+    content = forms.CharField(required=True, widget=forms.widgets.Textarea(attrs={'class': 'ribbitText'}))
+
+    def is_valid(self):
+        form = super(RibbitForm, self).is_valid()
+        for f in self.errors.iterkeys():
+            if f != '__all__':
+                self.fields[f].widget.attrs.update({'class': 'error ribbitText'})
+        return form
+
+    class Meta:
+        model = Ribbit
+        exclude = ('user',)
--- reviews/models.py
+++ reviews/models.py
@@ -1,9 +1,8 @@
 from __future__ import unicode_literals
 from django.db import models
 from django.contrib.auth.models import User
+import hashlib
 import numpy as np
-
-# Create your models here.
 
 class Game(models.Model):
     name = models.CharField(max_length=200)
@@ -28,3 +27,18 @@ class Review(models.Model):
     user_name = models.CharField(max_length=100)
     comment = models.CharField(max_length=200)
     rating = models.IntegerField(choices=RATING_CHOICES)
+
+
+class Ribbit(models.Model):
+    content = models.CharField(max_length=140)
+    user = models.ForeignKey(User)
+    creation_date = models.DateTimeField(auto_now=True, blank=True)
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User)
+    follows = models.ManyToManyField('self', related_name='followed_by', symmetrical=False)
+
+    def gravatar_url(self):
+        return "http://www.gravatar.com/avatar/%s?s=50" % hashlib.md5(self.user.email).hexdigest()
+
+User.profile = property(lambda u: UserProfile.objects.get_or_create(user=u)[0])
